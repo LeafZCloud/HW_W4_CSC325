f7dd334aa9e9b83c2b10398b145138c16e31fbf3
Hamdi Tekci <hamdit2006@outlook.com>
2026-02-10 11:39:07 -0500

    Added a print line

--- src/main/java/com/mycompany/agency/Executive.java
+++ src/main/java/com/mycompany/agency/Executive.java
@@ -14,7 +14,7 @@ public class Executive
             String socSecNumber, double rate)
     {
         super(eName, eAddress, ePhone, socSecNumber, rate);
-
+        System.out.println();
         bonus = 0;  // bonus has yet to be awarded
     }
 

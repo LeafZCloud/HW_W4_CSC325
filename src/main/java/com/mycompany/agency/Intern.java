b1a2f7a94cd761ff90885e3ff4179d3a4bf7e0ad
roelliott36 <ro.elliott36@gmail.com>
2026-02-10 19:53:34 -0500

    I did TODO 1 only because I wanted the error message to go away. All TODOs in Intern.java are done!

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -1,18 +1,17 @@
 package com.mycompany.agency;
 
+    // TODO 1: Make Intern a child of StaffEmployee
 
-
-    // TODO 1: Make Intern a child of StaffEmploee
-
-public class Intern 
+public class Intern extends StaffEmployee
 {
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
     //  information.
     //-----------------------------------------------------------------
+
     public Intern(String eName, String eAddress, String ePhone)
     {
-        
+        super(eName, eAddress, ePhone, "", 0.0);
     }
 
     //-----------------------------------------------------------------
@@ -20,6 +19,6 @@ public class Intern
     //-----------------------------------------------------------------
     public double pay()
     {
-      
+      return 0.0;
     }
 }
